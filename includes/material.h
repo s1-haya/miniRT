33c70bb92b78a675ea9e63bc2a12ddcd9d8f1687
Sawa <sunzecun@gmail.com>
2024-02-06 16:16:58 +0900

    parseの基本設計とshapeの構造体変更
    - 各物体の構造体からvoid ＊に変更
    - materialにgloss factor追加
    
    次回
    - void *の配列の作成、削除、使い方練習
    - 交点のリファクタリング
    - 実装する。

--- includes/material.h
+++ includes/material.h
@@ -6,7 +6,7 @@
 /*   By: hsawamur <hsawamur@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 20:55:12 by hsawamur          #+#    #+#             */
-/*   Updated: 2024/01/18 21:39:47 by hsawamur         ###   ########.fr       */
+/*   Updated: 2024/02/06 16:06:52 by hsawamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct s_material
 	t_color	ambient;
 	t_color	diffuse;
 	t_color	specular;
+	t_color	gloss_factor;
 	double	shininess;
 }	t_material;
 
