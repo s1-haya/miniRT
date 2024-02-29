497e9e16081be37155616fdbb6fd8afbcfe4c0d5
Sawa <sunzecun@gmail.com>
2024-02-29 11:11:08 +0900

    lightを線形リストで描画できるか確認

--- includes/light.h
+++ includes/light.h
@@ -19,9 +19,11 @@
 
 typedef struct s_light
 {
-	double		distance;
-	t_vector	point;
-	double		intensity;
+	double			distance;
+	t_vector		point;
+	double			intensity;
+	t_rgb			rgb;
+	struct s_light	*next;
 }	t_light;
 
 typedef struct s_ambient_light
@@ -30,6 +32,7 @@ typedef struct s_ambient_light
 	t_rgb	rgb;
 }	t_ambient_light;
 
-t_light	new_light(t_vector point, double intensity);
+// t_light	new_light(t_vector point, double intensity, t_rgb rgb);
+t_light	*new_light(t_vector point, double intensity, t_rgb rgb);
 
 #endif
